a6213075bc128ea21cc5c5e6b905b4b2fd22cb0f
Björn Persson <bjorn.persson@knowit.se>
2022-05-02 15:40:40 +0200

    updated mypages url

--- source/js/constants.ts
+++ source/js/constants.ts
@@ -1,3 +1,3 @@
 export const COLLECTPOLL_INTERVAL = 2000;
-export const MYPAGES_URL = '/mina-sidor/?authenticated=true';
+export const MYPAGES_URL = '/mitt-helsingborg/mina-sidor/?authenticated=true';
 export const SIGNOUT_URL = '/?signedOut=true';
